17aa3eb936d20bd4b339db03e45af9a52a67f2f8
Martin Marek <12614710+martin-marek@users.noreply.github.com>
2025-03-02 17:36:32 -0500

    main: remove device count

--- main.py
+++ main.py
@@ -3,15 +3,11 @@ from omegaconf import OmegaConf, DictConfig
 import train
 
 
-import os
-os.environ['XLA_FLAGS'] = '--xla_force_host_platform_device_count=8'
-
-
-@hydra.main(version_base=None, config_path="configs", config_name="local")
+@hydra.main(version_base=None, config_path='configs', config_name='local')
 def main(c: DictConfig):
     OmegaConf.set_struct(c, False)
     train.train_and_evaluate(c)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
